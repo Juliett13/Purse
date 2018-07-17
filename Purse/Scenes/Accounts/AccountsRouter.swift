0e6c293d9425b185206a9a24658b4a7e231fa15f
Juliett13 <k.julia_13@mail.ru>
2018-07-17 08:51:53 +0300

    added routers

---
+++ Purse/Scenes/Accounts/AccountsRouter.swift
@@ -0,0 +1,11 @@
+
+
+//
+//  Accountsrouter.swift
+//  Purse
+//
+//  Created by Kuroyan Juliett on 17.07.2018.
+//  Copyright © 2018 C3G9. All rights reserved.
+//
+
+import Foundation
---
+++ Purse/ViewRouter.swift
@@ -0,0 +1,9 @@
+//
+//  ViewProtocol.swift
+//  Purse
+//
+//  Created by Kuroyan Juliett on 17.07.2018.
+//  Copyright © 2018 C3G9. All rights reserved.
+//
+
+import Foundation
